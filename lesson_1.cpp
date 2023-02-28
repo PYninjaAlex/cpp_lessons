cf55c512dca6a1aae1dede7297ce0e8fd296b58b
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-02-28 18:50:50 +0300

    main.cpp renamed.

---
+++ lesson_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+
+void practice() 
+{
+    int i = 0;
+    while (true)
+    {
+        std::cout << "C++ while looop! " << i << "\n";
+        ++i;
+        if (i == 1001) 
+        {
+            break;
+        }
+    }
+    std::string a = "String in cpp, with #include <string>";
+    std::cout << "\n" << a << "\n";
+}
+
+
+int main()
+{
+    practice();
+    std::cout << "Code: 1 or 0: ";
+    int output;
+    std::cin >> output;
+    while ((output != 0) && (output != 1))
+    {
+        std::cout << "ERROR!\n";
+        std::cout << "Code: 1 or 0: ";
+        std::cin >> output;
+    }
+    std::cout << "Code: " << output << "\n\n";
+    std::cout << "Done!\n";
+    return 0;
+}
